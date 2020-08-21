700378de6012ded4e89b8d279518759694d011c3
Katelyn Wyatt <kwyatt@student04.cse.nd.edu>
2020-08-20 23:12:59 -0400

    Level 2 UML Methods Added

--- Mammal.java
+++ Mammal.java
@@ -1,7 +1,7 @@
 public class Mammal extends Animal {
 
     // Properties
-    String amtFir;
+    String amtFur;
     int litterSize;
 
 
@@ -10,5 +10,16 @@ public class Mammal extends Animal {
     // constructor
     // access
     // set
+
+    // print
+    public void printSubDetails() {
+        System.out.println(name + " has " + " fur.");
+        System.out.println("The " + species + "species can have up to " + litterSize + " children at one time.");
+    }
+
+
+    // other
+    public void warmBlooded() {System.out.println(name + " is warm blooded."); }
+    public void hasHair() {System.out.println(name + " is has hair because they are a mammal."); }
 
 }
--- Reptile.java
+++ Reptile.java
@@ -10,5 +10,28 @@ public class Reptile extends Animal {
     // constructor
     // access
     // set
+    // print
+    public void printSubDetails() {
+        // Legs
+        if (legs) {
+            System.out.println(name + " is a reptile with legs.");
+        }
+        else { 
+            System.out.println(name + " is a reptile without legs.");
+        }
+        // Webbed Feet
+        if (webbedFeet) {
+            System.out.println(name + " is a reptile with webbed feet.");
+        }
+        else {
+            System.out.println(name + " is a reptile without webbed feet.");
+        }
+    }
+
+    // other
+    public void needsExtHeat() { System.out.println("In order to survive, reptiles need an external heat source."); }
+
+    public void cantSweat() { System.out.println("Reptiles can't sweat."); }
+        
 
 }
