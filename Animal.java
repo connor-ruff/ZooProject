0f5e73031e29b946c01bcd91351e2ffe9c570513
Katelyn Wyatt <kwyatt@student04.cse.nd.edu>
2020-08-20 23:12:39 -0400

    Animal Methods Updated

--- Animal.java
+++ Animal.java
@@ -13,7 +13,57 @@ public class Animal {
 
     // Methods
     // constructor
+    public Animal (String cons_in, String name_in, int age_in, String habitat_in, String health_in, String spec_in, String diet_in, String desc_in) {
+    
+        conservationStat = cons_in;
+        name = name_in;
+        age = age_in;
+        habitat = habitat_in;
+        healthStat = health_in;
+        species = spec_in;
+        diet = diet_in;
+        desc = desc_in;    
+    }
+
     // access
+    String getConsStat() { return conservationStat; }
+    String getName() { return name; }
+    int getAge() { return age; }
+    String getHabitat() { return habitat; }
+    String getHealthStat() { return healthStat; }
+    String getSpecies() { return species; }
+    String getDiet() { return diet; }
+    String getDesc() { return desc; }
+
     // set
+    void setConsStat(String cons_in) { conservationStat = cons_in; }
+    void setAge(int age_in) { age = age_in; }
+    void setHealthStat(String health_in) { healthStat = health_in; }
+    void setDiet(String diet_in) { diet = diet_in; }
+    void setDesc(String desc_in) { desc = desc_in; }
+
+
+    // prints
+    public final void printDesc() {
+        System.out.println(desc);
+    }
+
+
+    public final void printGeneralDetails() {
+    
+        System.out.println("Name: " + name);
+        System.out.println("Species: " + species);
+        System.out.println("Age: " + age);
+        System.out.println("Health Status: " + healthStat);
+        System.out.println("Conservation Status: " + conservationStat);
+    
+    }
+
+
+    public abstract void printSubDetails();
+
+
+    // other
+    void final livesAtZoo() { System.out.println(name + " lives at the zoo!"); }
 
 }
