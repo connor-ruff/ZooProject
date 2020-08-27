aaf43dd46354334b303f08c9ccec574ce569dc4a
cbenyah <cbenyah@nd.edu>
2020-08-27 13:46:03 +0000

    incomplete attempt #2

--- zooDatabase.java
+++ zooDatabase.java
@@ -3,10 +3,17 @@
 public static class ZooDatabase{
 	
 	// make an empty animal array list that way u can add no matte what :P
+	static setupAnimals(){
+		int numAnimals = 6;
+		Animal[] alist = new Animal[6];
+		
+		System.out.println("\n");
+	}
+
 	// set up animals function that fills in the empy array 	
- 
+	 
 	//create function for the summary
-	static void summary(){
+	static void printSummary(){
 		System.out.println("Summary");
 		// how to ensure im on a new line 
 		//System.out.println("There are" + +  "animals in the zoo."); // fix this
@@ -20,14 +27,42 @@ public static class ZooDatabase{
 	}	
 		
 	// create function for the verbose 
-	static void verbose(){	
-		System.out.println("	Name	Species");
-		for(int i = 1; i < number of animals + 1; i++){ //fix this 
+	static void printVerbose(){	
+		int numAnimals = alist.length; 
 			
+		//System.out.println("	Name	Species");
+		for(int i = 0; i < numAnimals ; i++){ //fix this 
+			System.out.println((i+1) + ": " alist[i].tostring()); // check this 
 			
 		}
 		
 	}
+	// adding 
+	static Animal[] addAnimal(Animal a, Animal[] alist){
+		// check the following stuff
+		Animal anim;
+		String species = stdin. next();
+		String name = stdin.next(); 
+		
+		if(species.toLowerCase().equals(" " )){
+			anim = new (a) // incomplete
+		}
+		else if(species.toLowerCase().equals(" ")){
+		}
+		else {
+			
+		}
+	}
+	
+	
+	// deleting 
+	static Animal[] deleteAnimal(int index, Animal[] alist){}
+
+	
+	//checking 
+	static Animal[] checkDeleteAnimal(String namel, Animal[] alist){}
+	
+
 
 	public static void main(String[] args){
 		
