6c57746de3faafe23e0f3a752b88d9dd1cb363a8
Katelyn Wyatt <kwyatt@student04.cse.nd.edu>
2020-08-28 16:48:37 -0400

    Bird Update

--- Penguin.java
+++ Penguin.java
@@ -7,7 +7,32 @@ public class Penguin extends flightlessBird {
 
     // Methods
     // constructor
+    public Penguin(name_in) {
+        super("Not Threatened", name_in, "North Pole", "Healthy", "Penguin", "Fish", "She loves penguin sledding!", 6.2, true, true, "Narrow", "Swimming");
+        this.swimmingSpeed = 15.3;
+        this.timeUnderwater = 4.3;
+    }
+    
     // access
+    float getSpeed() { return this.swimmingSpeed; }
+    float getTime() { return this.timeUnderwater; }
+    
     // set
+    void setSpeed(sp_in) { this.swimmingSpeed = sp_in; }
+    void setTime(tm_in) { this.timeUnderwater = tm_in; }
+
+
+    // print
+    public void printSubDetails() {
+        System.out.println(this.getName() + " can swim up to " + this.swimmingSpeed + " mph.");
+        System.out.println(this.getName() + " can stay underwater up to " + this.timeUnderwater + " minutes.");
+    
+    // other
+    public void carriesEgg() { 
+        System.out.println("Penguins carry their eggs before they hatch.");
+    }
+    public void drinksSeaWater() { 
+        System.out.println("Penguins can drink water from the sea.");
+    }
 
 }
