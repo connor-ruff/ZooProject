34103c07e4328ce46e53cddfc8ba4a13e2d7fe48
kwyatt-dev <62321306+kwyatt-dev@users.noreply.github.com>
2020-08-30 10:56:01 -0400

    Delete testDriver.java

--- testDriver.java
+++
@@ -1,7 +0,0 @@
-public class testDriver {
-
-	public static void main (String [] args) {
-		SnowLeopard lep = new SnowLeopard("Bobby", "Snow Leopard") ;
-		System.out.println(lep.getName() + " " + lep.getSpecies());	
-	}
-}
